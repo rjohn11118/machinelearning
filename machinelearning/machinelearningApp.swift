385e888875c64addafbfda7ba37b6e11874ded60
River <river.johnson@stu.barren.kyschools.us>
2024-10-16 09:01:07 -0500

    Initial Commit

---
+++ machinelearning/machinelearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  machinelearningApp.swift
+//  machinelearning
+//
+//  Created by Student on 10/16/24.
+//
+
+import SwiftUI
+
+@main
+struct machinelearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
